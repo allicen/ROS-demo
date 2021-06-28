27509a353d52e88bd456a91e37a7ac12a237a7e7
allicen <ellena.hella@yandex.ru>
2021-06-28 14:34:40 +0500

    removed gitignored files

--- armbot/build/first_world/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/e/ROS-demo/armbot/src/first_world
-# Build directory: /home/e/ROS-demo/armbot/build/first_world
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- mybot_ws/build/mybot_navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/e/ROS-demo/mybot_ws/src/mybot_navigation
-# Build directory: /home/e/ROS-demo/mybot_ws/build/mybot_navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
