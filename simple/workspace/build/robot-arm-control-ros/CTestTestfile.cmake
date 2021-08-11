2453be4ca64f7db5f84c59edeea3e473a094eb0c
allicen <ellena.hella@yandex.ru>
2021-08-12 03:07:57 +0500

    Добавлены роботы

---
+++ simple/workspace/build/robot-arm-control-ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/e/ROS/ROS-demo/simple/workspace/src/robot-arm-control-ros
+# Build directory: /home/e/ROS/ROS-demo/simple/workspace/build/robot-arm-control-ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
